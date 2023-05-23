1f0eccbce5654ed800894b61e518a8b3371820fa
yydev10 <jym971024@gmail.com>
2023-05-24 00:36:09 +0900

    fix : main 화면에 listview 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter/widgets.dart';
 import 'package:inventory_mgmt/screen/main_screen.dart';
+import 'package:inventory_mgmt/screen/product_screen.dart';
 import 'package:inventory_mgmt/screen/start_screen.dart';
 import 'firebase_options.dart';
 
--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:inventory_mgmt/screen/product_screen.dart';
 
-class Item {
-  final int number;
-  final String product;
-  final int specific;
-  final int count;
-  final String project;
-
-  Item(this.number, this.product, this.specific, this.count, this.project);
-}
+import '../model/Item.dart';
 
 class MainScreen extends StatefulWidget{
   _MainScreenState createState() => _MainScreenState();
@@ -25,6 +18,21 @@ class _MainScreenState extends State<MainScreen> {
       Item(3, '케이블', 15, 3,'프로젝트1'),
       Item(4, '케이블', 15, 3,'프로젝트1'),
       Item(5, '케이블', 15, 3,'프로젝트1'),
+      Item(1, '케이블', 15, 3,'프로젝트1'),
+      Item(2, '케이블', 15, 3,'프로젝트1'),
+      Item(3, '케이블', 15, 3,'프로젝트1'),
+      Item(4, '케이블', 15, 3,'프로젝트1'),
+      Item(5, '케이블', 15, 3,'프로젝트1'),
+      Item(1, '케이블', 15, 3,'프로젝트1'),
+      Item(2, '케이블', 15, 3,'프로젝트1'),
+      Item(3, '케이블', 15, 3,'프로젝트1'),
+      Item(4, '케이블', 15, 3,'프로젝트1'),
+      Item(5, '케이블', 15, 3,'프로젝트1'),
+      Item(1, '케이블', 15, 3,'프로젝트1'),
+      Item(2, '케이블', 15, 3,'프로젝트1'),
+      Item(3, '케이블', 15, 3,'프로젝트1'),
+      Item(4, '케이블', 15, 3,'프로젝트1'),
+      Item(5, '케이블', 15, 3,'프로젝트1'),
     ];
 
     return MaterialApp(
@@ -36,18 +44,20 @@ class _MainScreenState extends State<MainScreen> {
           ),
           child: Column(
             children: [
-              const SizedBox(
-                height: 40,
-              ),
-              Padding(
-                padding: EdgeInsets.all(5.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Search',
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(),
+              SizedBox(height: 60),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Search',
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
               Row(
                   children: <Widget>[
@@ -60,25 +70,44 @@ class _MainScreenState extends State<MainScreen> {
               ),
               Expanded(
                 child: ListView.builder(
+                  padding: EdgeInsets.only(top: 0),
                   itemCount: items.length,
                   itemBuilder: (context, index) {
                     final item = items[index];
-                    return Container(
-                      padding: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(color: Colors.black12),
+                    return GestureDetector(
+                      onTap: (){
+                        Navigator.push(
+                            context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductScreen(item: items[index]),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black12),
+                          ),
                         ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('${item.number}'),
-                          Text('${item.product}'),
-                          Text('${item.specific}'),
-                          Text('${item.count}'),
-                          Text('${item.project}'),
-                        ],
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('${item.number}'),
+                            Text('${item.product}'),
+                            Text('${item.specific}'),
+                            Container(
+                              width: 30,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: '${item.count}',
+                                  isDense: true,
+                                ),
+                              ),
+                            ),
+                            Text('${item.project}'),
+                          ],
+                        ),
                       ),
                     );
                   },
@@ -88,9 +117,7 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Plus button logic
-          },
+          onPressed: (){},
           child: Icon(Icons.add),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
